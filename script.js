9844fa9bbfa5c840ab68b6d825980dd3caefe866
mark.paul <markpaulbusiness66@gmail.com>
2023-11-05 13:40:45 +0300

    ARRAY METHODS

--- script.js
+++ script.js
@@ -80,3 +80,36 @@ let arr = ["a", "b", "c", "d", "e"];
 //SLICE METHOD.
 
 console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log("-----SHALLOW COPPY-----");
+console.log(arr.slice());
+console.log([...arr]);
+
+//SPLICE METHOD - mutates the array.
+
+console.log(arr.splice(2));
+console.log(arr);
+
+console.log(arr.splice(1, 2));
+console.log(arr);
+
+// REVERSE
+
+arr = ["a", "b", "c", "d", "e"];
+
+const arr2 = ["j", "i", "h", "g", "f"];
+
+console.log(arr2.reverse());
+console.log(arr2);
+
+//Concat methods
+
+const letters = arr.concat(arr2);
+
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//JOIN
+console.log(letters.join(" - "));
