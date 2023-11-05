48048c0cfd20994579c0415cb4b1e3b27c906ff2
mark.paul <markpaulbusiness66@gmail.com>
2023-11-05 22:55:13 +0300

    FOREACH LOOP

--- script.js
+++ script.js
@@ -130,4 +130,20 @@ console.log("Jonas".at(-1));
 
 //LOOPING ARRAYS FOREACH.
 
-movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+movements.push(200, 450, -400, 3000, -650, -130, 70, 1300);
+
+// for (const movement of movements){
+//     if(movement > 0){
+//         console.log(`You deposited ${movement}`)
+//     } else {
+//         console.log(`You withdrew ${Math.abs(movement)}`)
+//     }
+// }
+console.log(movements);
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdraw ${Math.abs(movement)}`);
+  }
+});
