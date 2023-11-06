ab488ff96b2606e135527974f77b3fca16ce1e0a
mark.paul <markpaulbusiness66@gmail.com>
2023-11-06 16:04:29 +0300

    IMPROVED THE LOGIC INSIDE THE MAP METHOD

--- script.js
+++ script.js
@@ -259,11 +259,9 @@ for (const mov of movements) {
 console.log(newArr);
 
 const movementsDescriptions = movements.map((mov, i, arr) => {
-  if (mov > 0) {
-    return `Movement ${i + 1}: You deposited ${mov}`;
-  } else {
-    return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
-  }
+  `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+    mov
+  )}`;
 });
 
 console.log(movementsDescriptions.join("\n"));
